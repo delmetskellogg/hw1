b80030f80aeb730e1594e7f2ca6233252d7da454
delmetskellogg <david.elmets@kellogg.northwestern.edu>
2022-01-21 16:47:37 +0000

    domain model partial completion

--- kmdb.sql
+++ kmdb.sql
@@ -67,10 +67,38 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS cast;
+
+
+
 
 -- Create new tables, according to your domain model
 -- TODO!
+CREATE TABLE people (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+);
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  release_year INTEGER,
+  mpaa_rating TEXT,
+  director_id TEXT
+);
+
+
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movie_id TEXT,
+  person_id TEXT,
+  movie_character TEXT
+);
+
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
